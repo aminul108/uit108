e91fe4e6dcfb1c142b40bd9108cc785d164350e6
Md. Aminul Islam <114870759+aminul108@users.noreply.github.com>
2022-10-05 20:19:15 +0600

    Class2.py

---
+++ class2.py
@@ -0,0 +1,46 @@
+a=5
+print(a)
+b=3
+c=-a+b
+print(c)
+print(type(c))
+d=a**b
+print(d)
+e=b**4
+print(e)
+print(type(e))
+f=16
+g=f//a
+print(g)
+print(float(g))
+print(str(g))
+a+=a
+print(a)
+
+a-=b
+print(a)
+
+h=4
+h*=2
+print(h)
+h&=3
+h&=2
+print(h)
+
+i=5
+j=6
+print(i==j)
+#Because i is not equal to j
+
+print(i!=j)
+print(i>j)
+print(i<j)
+k= 7
+l=7
+print(k==l)
+print(k>=l)
+print(l<=k)
+print(k>4 and l>3)
+print(k<4 or l>6)
+print(not(k>6 and l<9))
+print(not(k>9 and l<4))
